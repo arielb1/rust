0e535dc2bb8dbbb2c282dd3d111234063146e4ba
Ariel Ben-Yehuda <ariel.byd@gmail.com>
2015-05-26 02:28:04 +0300

    Remove ObjectCastMap

--- src/librustc_typeck/coherence/mod.rs
+++ src/librustc_typeck/coherence/mod.rs
@@ -285,7 +285,8 @@ impl<'a, 'tcx> CoherenceChecker<'a, 'tcx> {
             }
             _ => {
                 self.crate_context.tcx.sess.span_bug(item.span,
-                                                     "can't convert a non-impl to an impl");
+                                                     "can't convert a non-impl \
+                                                      to an impl");
             }
         }
     }
